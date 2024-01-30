1309f4c64701d17e595ae926d129bcf13bcc0c00
OliveMonk <gp7.official@gmail.com>
2024-01-30 17:19:16 +0530

    added organization handler and auth handlers

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,15 @@
+'use client';
 
-import { UserButton } from "@clerk/nextjs";
+import { useOrganization } from "@clerk/nextjs";
+import EmptyOrg from "./_components/empty-org";
 
 export default function DashboardPage() {
+
+  const {organization}  = useOrganization();
+
   return (
-    <div className="flex flex-col gap-y-4">
-      <h1 className="text-2xl font-bold">Dashboard</h1>
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+        {!organization ? <EmptyOrg/> : <h1>Board List</h1>}
     </div>
   );
 }
