e1ffb9618f592bcae9f3c7f10dcd93a0c09d0a78
OliveMonk <gp7.official@gmail.com>
2024-01-31 17:07:26 +0530

    added boards and its layout

---
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -0,0 +1,18 @@
+const Toolbar = () => {
+  return (
+    <div className=" absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 ">
+      <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md ">
+        <div>pencil</div>
+        <div>Square</div>
+        <div>circle</div>
+        <div>eraser</div>
+      </div>
+      <div className=" bg-white rounded-md p-1.5 flex flex-col items-center shadow-md ">
+        <div>undo</div>
+        <div>redo</div>
+      </div>
+    </div>
+  );
+};
+
+export default Toolbar;
